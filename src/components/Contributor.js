fc7623d8c2cbe1ad24f34e9ebdf979c9b1c0d8d5
Reymer Fricas <reymerfmoscoso@gmail.com>
2019-09-29 21:52:17 -0400

    Ajust to Contributor component.

--- src/components/Contributor.js
+++ src/components/Contributor.js
@@ -3,35 +3,33 @@ import ReactDOM from "react-dom";
 import bootstrap from "bootstrap/dist/css/bootstrap.css";
 import fontawesome from "font-awesome/css/font-awesome.css";
 
-class Contributor extends React.Component{
+class Contributor extends React.Component {
     render(){
         return (
-            <div class="card mb-3">
-                <div className="row no-gutters">
-                    <div className="col-md-4">
-                        <img src={this.props.avatar} alt="GitHub Avatar"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">Contributor username</h5>
-                            <p className="card-text">
-                                Number of contributions
-                            </p>
-                            <p className="card-text">
-                                <small className="text-muted">
-                                    <a href={this.props.userLink}>
-                                        View profile
-                                    </a>
-                                </small>
-                            </p>
+            <div>
+                <div class="card mb-3">
+                    <div className="row no-gutters">
+                        <div className="col-md-4">
+                            <img src={this.props.avatar} alt="GitHub Avatar"></img>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">Contributor username</h5>
+                                <p className="card-text">
+                                    Number of contributions
+                                </p>
+                                <p className="card-text">
+                                    <small className="text-muted">
+                                        <a href={this.props.userLink}>
+                                            View profile
+                                        </a>
+                                    </small>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <button className="btn btn-success">Show 5 more</button>
-                </div>
             </div>
-            
         )
     }
 }
